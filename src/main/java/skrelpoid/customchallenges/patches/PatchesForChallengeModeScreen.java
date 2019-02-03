0779db02cc8977c3e6bdd35dcd4df7399c9f40dc
Dustin <dustin.kristen2000@yahoo.de>
2019-02-03 18:59:09 +0100

    Up and down in menupanel on controller

--- src/main/java/skrelpoid/customchallenges/patches/PatchesForChallengeModeScreen.java
+++ src/main/java/skrelpoid/customchallenges/patches/PatchesForChallengeModeScreen.java
@@ -1,5 +1,6 @@
 package skrelpoid.customchallenges.patches;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
 import com.evacipated.cardcrawl.modthespire.lib.Matcher.MethodCallMatcher;
@@ -9,6 +10,9 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.Hitbox;
+import com.megacrit.cardcrawl.helpers.controller.CInputActionSet;
 import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
 import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen.CurScreen;
 import com.megacrit.cardcrawl.screens.mainMenu.SaveSlotScreen;
@@ -67,6 +71,26 @@ public class PatchesForChallengeModeScreen {
 			public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
 				return LineFinder.findInOrder(ctMethodToPatch,
 						new MethodCallMatcher(SaveSlotScreen.class, "render"));
+			}
+		}
+	}
+
+	@SpirePatch(clz = MainMenuScreen.class, method = "updateMenuPanelController")
+	public static class UpdateController {
+		@SpirePostfixPatch
+		public static void Postfix(MainMenuScreen screen) {
+			if (!Settings.isControllerMode) {
+				return;
+			}
+			Hitbox hb = screen.panelScreen.panels.get(0).hb;
+			if (CInputActionSet.up.isJustPressed()
+					|| CInputActionSet.altUp.isJustPressed()) {
+				Gdx.input.setCursorPosition((int) screen.panelScreen.panels.get(1).hb.cX,
+						Settings.HEIGHT - (int) screen.panelScreen.panels.get(1).hb.cY);
+			} else if (CInputActionSet.down.isJustPressed()
+					|| CInputActionSet.altDown.isJustPressed()) {
+				Gdx.input.setCursorPosition((int) hb.cX,
+						Settings.HEIGHT - (int) hb.cY);
 			}
 		}
 	}
